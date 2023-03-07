133a52cc9e6d3d8599c8dec0e88d047d464052f7
imjivoi <lezhnev@giftery.ru>
2023-03-06 21:10:28 -0300

    fro test

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,41 +1,54 @@
 <template>
-  <div
-    class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700"
-  >
-    <form class="space-y-6" action="#">
-      <h5 class="text-xl font-medium text-gray-900 dark:text-white">Sign in to our platform</h5>
-      <div>
-        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-        <input
-          id="email"
-          type="email"
-          name="email"
-          class="input input-primary input-bordered"
-          placeholder="name@company.com"
-          required
-        />
-      </div>
-      <div>
-        <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          placeholder="••••••••"
-          class="input input-primary input-bordered"
-          required
-        />
-      </div>
-      <button type="submit" class="btn btn-primary btn-full">Login to your account</button>
-      <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-        Not registered?
-        <nuxt-link to="/auth/register" class="text-primary hover:underline">Create account</nuxt-link>
-      </div>
-    </form>
+  <div class="card w-full mx-auto max-w-sm rounded-2xl bg-base-100 shadow-xl sm:p-6 md:p-8">
+    <div class="card-body">
+      <h3 class="card-title">Sign in to our platform</h3>
+      <form class="space-y-6" @submit.prevent>
+        <div>
+          <label for="email" class="block mb-2 text-sm font-medium">Your email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            class="input input-primary input-bordered"
+            placeholder="name@company.com"
+            required
+          />
+        </div>
+        <div>
+          <label for="password" class="block mb-2 text-sm font-medium">Your password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            class="input input-primary input-bordered"
+            required
+          />
+        </div>
+        <button type="submit" class="btn btn-primary btn-full" @click="login">Login to your account</button>
+        <div class="text-sm font-medium ">
+          Not registered?
+          <nuxt-link to="/auth/register" class="text-primary hover:underline">Create account</nuxt-link>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
-definePageMeta({
-  layout: 'auth',
-})
+const supabase = useSupabaseClient()
+
+const email = ref('')
+const password = ref('')
+const isLoading = ref(false)
+
+const login = async () => {
+  isLoading.value = true
+  try {
+    await supabase.auth.signInWithPassword({ email: email.value, password: password.value })
+    await navigateTo('/dashboard')
+  } catch (error) {
+  } finally {
+    isLoading.value = false
+  }
+}
 </script>
--- shared/layouts/auth.vue
+++ shared/layouts/auth.vue
@@ -1,4 +1,3 @@
 <template>
-  <shared-ui-header></shared-ui-header>
   <main class="h-full flex items-center justify-center"><slot /></main>
 </template>
