601b635091974045824f9436112e889976a23207
imjivoi <lezhnev@giftery.ru>
2023-03-07 17:08:53 -0300

    test

--- app/middleware/alias.ts
+++ app/middleware/alias.ts
@@ -1,15 +1,19 @@
 export default defineNuxtRouteMiddleware(async ({ params }) => {
-  const device = useDevice()
-
-  const headers = useRequestHeaders(['cookie']) as Record<string, string>
-  const response = await $fetch(`/api/links/alias/${params.alias}`, {
-    method: 'GET',
-    headers,
-  })
-  if (response?.original_url) {
-    return await navigateTo(response.original_url, {
-      external: true,
+  try {
+    const headers = useRequestHeaders(['cookie']) as Record<string, string>
+    const response = await $fetch(`/api/links/alias/${params.alias}`, {
+      method: 'GET',
+      headers,
     })
+    if (response?.original_url) {
+      return await navigateTo(response.original_url, {
+        external: true,
+      })
+    }
+  } catch (error) {
+    console.log(error)
+    throw createError(error)
   }
+
   return navigateTo('/')
 })
