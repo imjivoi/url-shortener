8c85f18b87f9abd6abd7c9d5c7d2c34f833766ba
imjivoi <lezhnev@giftery.ru>
2023-03-07 16:59:20 -0300

    test

--- server/model/statistic/create-click.ts
+++ server/model/statistic/create-click.ts
@@ -14,34 +14,32 @@ export const createClick = async (event: H3Event, linkId: string) => {
     geo = process.env.NODE_ENV === 'production' ? geolocation(req) : {}
     ua = getHeader(event, 'user-agent')
     referer = getHeader(event, 'referer')
+    const { error } = await client.from('click').insert([
+      {
+        link_id: linkId,
+        country: geo?.country || 'Unknown',
+        city: geo?.city || 'Unknown',
+        region: geo?.region || 'Unknown',
+        latitude: geo?.latitude || 'Unknown',
+        longitude: geo?.longitude || 'Unknown',
+        ua: ua?.ua || 'Unknown',
+        browser: ua?.browser?.name || 'Unknown',
+        browser_version: ua?.browser?.version || 'Unknown',
+        engine: ua?.engine?.name || 'Unknown',
+        engine_version: ua?.engine?.version || 'Unknown',
+        os: ua?.os?.name || 'Unknown',
+        os_version: ua?.os?.version || 'Unknown',
+        device: ua?.device?.type ? ua?.device?.type : 'Desktop',
+        device_vendor: ua?.device?.vendor || 'Unknown',
+        device_model: ua?.device?.model || 'Unknown',
+        cpu_architecture: ua?.cpu?.architecture || 'Unknown',
+        bot: ua?.isBot,
+        referer: referer || '(direct)',
+        referer_url: referer || '(direct)',
+      },
+    ])
   } catch (error) {
     console.log(error)
+    return error
   }
-
-  const { error } = await client.from('click').insert([
-    {
-      link_id: linkId,
-      country: geo?.country || 'Unknown',
-      city: geo?.city || 'Unknown',
-      region: geo?.region || 'Unknown',
-      latitude: geo?.latitude || 'Unknown',
-      longitude: geo?.longitude || 'Unknown',
-      ua: ua?.ua || 'Unknown',
-      browser: ua?.browser?.name || 'Unknown',
-      browser_version: ua?.browser?.version || 'Unknown',
-      engine: ua?.engine?.name || 'Unknown',
-      engine_version: ua?.engine?.version || 'Unknown',
-      os: ua?.os?.name || 'Unknown',
-      os_version: ua?.os?.version || 'Unknown',
-      device: ua?.device?.type ? ua?.device?.type : 'Desktop',
-      device_vendor: ua?.device?.vendor || 'Unknown',
-      device_model: ua?.device?.model || 'Unknown',
-      cpu_architecture: ua?.cpu?.architecture || 'Unknown',
-      bot: ua?.isBot,
-      referer: referer || '(direct)',
-      referer_url: referer || '(direct)',
-    },
-  ])
-
-  console.log(error)
 }
