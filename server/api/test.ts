101ecfcd695a0e0d9698ae5a14098602a404c1c3
imjivoi <lezhnev@giftery.ru>
2023-03-07 18:21:25 -0300

    test

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,14 +1,12 @@
-import { geolocation } from '@vercel/edge'
+// import { geolocation } from '@vercel/edge'
 
 export default defineEventHandler((event) => {
   try {
-    const geo = process.env.NODE_ENV === 'production' ? geolocation(event.node.req) : {}
-
-    console.log(getHeader(event, 'user-agent'))
-
-    return {
-      geo,
-    }
+    // const geo = process.env.NODE_ENV === 'production' ? geolocation(event.node.req) : {}
+    // console.log(getHeader(event, 'user-agent'))
+    // return {
+    //   geo,
+    // }
   } catch (error) {
     return {
       error: error?.message,
