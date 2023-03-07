9e65c08f7b44741cb7c4474813ffc46203607186
imjivoi <lezhnev@giftery.ru>
2023-03-07 17:21:37 -0300

    test

--- server/api/test.ts
+++ server/api/test.ts
@@ -1,3 +1,11 @@
+import { geolocation } from '@vercel/edge'
+
 export default defineEventHandler((event) => {
+  const geo = process.env.NODE_ENV === 'production' ? geolocation(event.node.req) : {}
+
   console.log(getHeader(event, 'user-agent'))
+
+  return {
+    geo,
+  }
 })
