bb1a08e08c11a611e137f67ca7dbd46ff9ca7134
imjivoi <lezhnev@giftery.ru>
2023-03-14 20:05:11 -0300

    fix: serviceRoleClient for getCachedLinkByAlias

--- server/model/link/get-by-alias.ts
+++ server/model/link/get-by-alias.ts
@@ -1,10 +1,10 @@
 import { H3Event } from 'h3'
 
-import { supabaseClient } from 'server/supabase'
+import { serviceRoleClient } from 'server/supabase'
 
 export const getCachedLinkByAlias = cachedFunction(
   async (event: H3Event, alias: string) => {
-    const client = supabaseClient(event)
+    const client = serviceRoleClient(event)
     const { data } = await client.from('links').select().eq('alias', alias).single()
 
     return data
