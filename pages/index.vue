5b519ada5d6a67516ab86e65be0f8262d04bf150
imjivoi <jivoi001@gmail.com>
2023-08-15 20:13:51 -0300

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="py-28">
     <div class="text-center mb-10">
-      <h1 class="text-5xl font-bold mb-8">
-        Simple way to short
-        <span class="bg-clip-text bg-gradient-to-r from-purple-400 to-blue-600 text-transparent">your link</span>
-      </h1>
+      <h1 class="text-5xl font-bold mb-8">Simple way to short your link</h1>
       <p class="text-xl max-w-lg mx-auto text-gray-500">
         Create custom short links that are perfect for sharing on social media, email, and more - and start seeing
         results today
