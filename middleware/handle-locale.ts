a68cfa82fda02c38cdba0ad191c4db3b779cf5c4
imjivoi <jivoi001@gmail.com>
2023-08-20 21:41:16 -0300

    comment handle-locale temporaly

--- middleware/handle-locale.ts
+++ middleware/handle-locale.ts
@@ -1,10 +1,14 @@
 export default defineNuxtRouteMiddleware((to) => {
-  const nuxtApp = useNuxtApp()
-  const locale = nuxtApp.$i18n.locale.value
-  const defaultLocale = nuxtApp.$i18n.defaultLocale
-  const localeCodes = nuxtApp.$i18n.locales.value.map((locale) => locale.code)
-  const isNotDefaultLocale = localeCodes.includes(to.name as string)
+  // const nuxtApp = useNuxtApp()
+  // const locale = nuxtApp.$i18n.locale.value
+  // const defaultLocale = nuxtApp.$i18n.defaultLocale
+  // const localeCodes = nuxtApp.$i18n.locales.value.map((locale) => locale.code)
+  // const isNotDefaultLocale = localeCodes.includes(to.name as string)
+  // // console.log('isNotDefaultLocale && locale !== to.name: ', isNotDefaultLocale && locale !== to.name)
+  // // console.log('locale !== defaultLocale: ', locale !== defaultLocale)
+  // console.log('locale: ', locale)
+  // console.log('----------------')
 
-  if (isNotDefaultLocale && locale !== to.name && to.name !== defaultLocale) return navigateTo('/')
-  if (to.name === 'index' && locale !== defaultLocale) return navigateTo(`/${locale}`)
+  // if (isNotDefaultLocale && locale !== to.name) return navigateTo('/')
+  // if (to.name === 'index' && locale !== defaultLocale) return navigateTo(`/${locale}`)
 })
