1ac92f80825c7998394be707ab15375b850c60ca
imjivoi <jivoi001@gmail.com>
2024-02-27 16:25:30 -0300

    fix

--- middleware/domain-redirect.global.ts
+++ middleware/domain-redirect.global.ts
@@ -1,11 +1,11 @@
 export default defineNuxtRouteMiddleware(async (to) => {
   const routeName = to.name
   const {
-    public: { appDomainUrl },
+    public: { appUrl, appDomainUrl },
   } = useRuntimeConfig()
   const host = useRequestHeader('host') || window.location.host
 
   if (routeName !== 'alias' && host !== appDomainUrl) {
-    return navigateTo(appDomainUrl + to.path, { replace: true, redirectCode: 309, external: true })
+    return navigateTo(appUrl + to.path, { replace: true, redirectCode: 309, external: true })
   }
 })
