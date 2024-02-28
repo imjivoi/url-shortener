fb93547f49ca18cca614459b49a36eac2b3cbc34
imjivoi <jivoi001@gmail.com>
2024-02-27 23:21:28 -0300

    fix

--- server/api/links/[id]/index.get.ts
+++ server/api/links/[id]/index.get.ts
@@ -2,9 +2,9 @@ import * as v from 'valibot'
 
 import { getLinkById } from '../../../services'
 
-export default defineAuthEventHandler(async (event) => {
+export default defineAuthEventHandler(async (event, user) => {
   const { id } = await useValidatedParams(event, v.objectAsync({ id: v.string([v.uuid()]) }))
 
-  const data = await getLinkById(id)
+  const data = await getLinkById(id, user.id)
   return { ...data, clicks: data.clicks && Array.isArray(data.clicks) && data.clicks[0].count }
 })
