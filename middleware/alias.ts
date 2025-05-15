49f1b28021a22968561e20df65dab755694de091
imjivoi <jivoi001@gmail.com>
2025-05-15 12:59:12 -0300

    Revert "ads on"
    This reverts commit 06c29d5a2fc2d1bbe77055cb04a3b547fe0b5fbd.

--- middleware/alias.ts
+++ middleware/alias.ts
@@ -33,21 +33,23 @@ export default defineNuxtRouteMiddleware(async ({ params }) => {
     const isLinkOlderThan2Days = new Date(link.created_at).getTime() < Date.now() - 2 * 24 * 60 * 60 * 1000
     if (!isCrawler(headers['user-agent'])) {
       // Check if ads have been viewed today
-      const adsViewedCookie = useCookie('ads-viewed', {
-        path: '/',
-        maxAge: 86400, // 24 hours
-      })
-      const currentDate = new Date().toDateString()
-      // If ads haven't been viewed today, redirect to ads page
-      if (adsViewedCookie.value !== currentDate) {
-        // Set cookie to mark ads as viewed for today
-        adsViewedCookie.value = currentDate
+      // no ads because of monetag sucks
+      // const adsViewedCookie = useCookie('ads-viewed', {
+      //   path: '/',
+      //   maxAge: 86400, // 24 hours
+      // })
+      // const currentDate = new Date().toDateString()
+      // console.log('adsViewedCookie: ', adsViewedCookie.value)
+      // // If ads haven't been viewed today, redirect to ads page
+      // if (adsViewedCookie.value !== currentDate) {
+      //   // Set cookie to mark ads as viewed for today
+      //   adsViewedCookie.value = currentDate
 
-        // Only redirect to ads page if not already on the ads page and the link is older than 2 days
-        if (!useRoute().path.endsWith('/ads') && isLinkOlderThan2Days) {
-          return navigateTo(`/${params.alias}/ads`)
-        }
-      }
+      //   // Only redirect to ads page if not already on the ads page and the link is older than 2 days
+      //   if (!useRoute().path.endsWith('/ads') && isLinkOlderThan2Days) {
+      //     return navigateTo(`/${params.alias}/ads`)
+      //   }
+      // }
 
       return navigateTo(link.original_url, {
         external: true,
